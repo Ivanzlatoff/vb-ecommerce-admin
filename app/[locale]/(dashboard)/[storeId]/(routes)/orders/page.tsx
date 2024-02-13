6c229326cbfcfff8d811d9b6317c281b157d3654
Ivan Zlatov <ivanzlatoff@gmail.com>
2024-02-13 19:51:38 +0000

    ...

--- app/[locale]/(dashboard)/[storeId]/(routes)/orders/page.tsx
+++ app/[locale]/(dashboard)/[storeId]/(routes)/orders/page.tsx
@@ -46,7 +46,10 @@ const OrdersPage = async ({
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <OrderClient data={formattedOrders} />
+        {!orders
+          ? <div>Loading orders...</div>
+          : <OrderClient data={formattedOrders} />
+        }
       </div>
     </div>
   )
