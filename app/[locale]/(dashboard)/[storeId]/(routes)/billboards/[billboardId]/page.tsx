1a1c21be4391949114c0706098946abc582b8111
Ivan Zlatov <ivanzlatoff@gmail.com>
2025-05-07 21:11:00 +0100

    .

--- app/[locale]/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ app/[locale]/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -8,7 +8,7 @@ const BillboardPage = async ({
 }: {
   params: { billboardId: string };
 }) => {
-  const { billboardId } = params;
+  const { billboardId } = await Promise.resolve(params);
   const billboard = await prismadb.billboard.findUnique({
     where: {
       id: billboardId
